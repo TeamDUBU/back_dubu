0d1be3829e13dea8217fa7145ceb790309c64f58
JsHyun0 <hyunjiseung0x@gmail.com>
2024-05-29 18:11:45 +0900

    2024-05-29

--- src/klaytnService.js
+++ src/klaytnService.js
@@ -3,6 +3,7 @@ const Caver = require("caver-js");
 const CaverExt = require("caver-js-ext-kas");
 const caver = new Caver(process.env.KLAYTN_NODE_URL);
 const abiNFT = require("../ABI_NFT.json");
+const abiDUBU = require("../ABI_DUBU.json");
 const caverExt = new CaverExt(
   1001,
   process.env.KAS_ACCESS_ID,
@@ -10,16 +11,36 @@ const caverExt = new CaverExt(
 );
 
 const NFTcontract = new caver.contract(abiNFT, process.env.NFT_ADDR);
+const DUBUcontract = new caver.contract(abiDUBU, process.env.DUBU_ADDR);
 
 async function useGetVault(tokenID) {
+  const res = await NFTcontract.methods.getVault(tokenID).call();
+  return res;
+}
+async function useGetTokensListAll() {
+  try {
+    const res = await NFTcontract.methods.getTokensListAll().call();
+    return res;
+  } catch (error) {
+    console.error("Error calling getTokensListAll:", error);
+  }
+}
+async function useGetApproved(tokenID) {
   return await NFTcontract.methods
-    .getVault(tokenID)
+    .getApproved(tokenID)
     .call()
     .then(async (res) => {
       return res;
     });
 }
-
+async function useGetAllHistory(tokenID, hosu) {
+  return await NFTcontract.methods
+    .getAllHistory(tokenID, hosu)
+    .call()
+    .then(async (res) => {
+      return res;
+    });
+}
 async function useHosuData(tokenID, hosu) {
   return await NFTcontract.methods
     .hosuData(tokenID, hosu)
@@ -45,6 +66,7 @@ async function useHosuDataArray(tokenID, index) {
       return res;
     });
 }
+
 async function useGetHosuDataArray(tokenID) {
   return await NFTcontract.methods
     .getHosuDataArray(tokenID)
@@ -54,25 +76,70 @@ async function useGetHosuDataArray(tokenID) {
     });
 }
 async function useHistoryData(tokenID, hosu, index) {
-  return await NFTcontract.methods
+  return await NFTcontract.methods // 해당 회차의 거래 정보 조회
     .historyData(tokenID, hosu, index)
     .call()
     .then(async (res) => {
       return res;
     });
 }
-// test area
-//tokenID : 115901070011019002
-async function testFunction() {
-  const out = await useHistoryData("1159010700110190021", 203, 1);
-  console.log(out);
+async function useGetHistoryData(tokenID, hosu, index) {
+  return await NFTcontract.methods // 모든 거래 내역 조회
+    .getHistoryData(tokenID, hosu)
+    .call()
+    .then(async (res) => {
+      return res;
+    });
 }
-testFunction();
+///////DUBU
+async function useGetAgentsListAll() {
+  return await DUBUcontract.methods
+    .getAgentsListAll()
+    .call()
+    .then(async (res) => {
+      return res;
+    });
+}
+
+async function useItem_List(tokenID, hosu) {
+  return await DUBUcontract.methods
+    .item_list(tokenID, hosu)
+    .call()
+    .then(async (res) => {
+      return res;
+    });
+}
+
+async function useGetItemsListAll() {
+  // return await DUBUcontract.methods
+  //   .getItemsListAll()
+  //   .call()
+  //   .then(async (res) => {
+  //     return res;
+  //   });
+  const res = await DUBUcontract.methods.getItemsListAll().call();
+  return res;
+}
+// test area
+//tokenID : 1159010700110190021
+// async function testFunction() {
+//   const out = await useGetTokensListAll();
+//   console.log(out);
+// }
+
+//testFunction();
 module.exports = {
+  useGetTokensListAll,
   useGetVault,
+  useGetApproved,
+  useGetAllHistory,
   useGetHosuData,
+  useGetHistoryData,
   useGetHosuDataArray,
   useHistoryData,
   useHosuData,
   useHosuDataArray,
+  useGetAgentsListAll,
+  useItem_List,
+  useGetItemsListAll,
 };
